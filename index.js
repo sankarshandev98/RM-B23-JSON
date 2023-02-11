d1a30e0ea7937c466d3abf94500c533eb5c90ba2
sankarshandev98 <gsankar164@gmail.com>
2023-02-11 12:34:11 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
 const path = require("path");
-
+//
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
